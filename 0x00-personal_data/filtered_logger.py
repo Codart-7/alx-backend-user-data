34896494746d85ff0a2b762e0acf8ee0ffd4a556
Codart-7 <iconmoa@gmail.com>
2023-02-02 19:04:09 +0100

    [fix] Q0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
-"""A function that returns an obfuscated log message"""
+""""0. Regex-ing"""
 import re
-import typing
+from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                separator: str) -> str:
-    """obfuscator function"""
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """a function called filter_datum that returns the
+    log message obfuscated"""
     for field in fields:
         message = re.sub(field + "=" + ".*?" + separator,
-                        field + "=" + redaction + separator,
-                        message)
+                         field + "=" + redaction + separator,
+                         message)
     return message
